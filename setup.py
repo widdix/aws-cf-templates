2198bc975aae0bfd9970058ed3ea27bc595d6560
Michael Wittig <post@michaelwittig.info>
2017-04-27 19:13:29 +0200

    no message

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import os
 class PreProcess(install):
   def run(self):
     install.run(self)
-    print("sed -i \"\" \"s/__VERSION__/" + os.environ['READTHEDOCS_VERSION'] + "/g\" docs/*.md")
-    os.system("sed -i \"\" \"s/__VERSION__/" + os.environ['READTHEDOCS_VERSION'] + "/g\" docs/*.md")
+    print("sed -i \"s/__VERSION__/" + os.environ['READTHEDOCS_VERSION'] + "/g\" docs/*.md")
+    os.system("sed -i \"s/__VERSION__/" + os.environ['READTHEDOCS_VERSION'] + "/g\" docs/*.md")
 
 setup(
   name="docs",
