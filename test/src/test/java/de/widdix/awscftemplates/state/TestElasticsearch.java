58ff140df68206b36dd4cd3e1e025649eb15920f
Michael Wittig <post@michaelwittig.info>
2018-02-26 16:30:57 +0100

    added state templates

---
+++ test/src/test/java/de/widdix/awscftemplates/state/TestElasticsearch.java
@@ -0,0 +1,40 @@
+package de.widdix.awscftemplates.state;
+
+import com.amazonaws.services.cloudformation.model.Parameter;
+import de.widdix.awscftemplates.ACloudFormationTest;
+import org.junit.Test;
+
+public class TestElasticsearch extends ACloudFormationTest {
+
+    @Test
+    public void test() {
+        final String vpcStackName = "vpc-2azs-" + this.random8String();
+        final String clientStackName = "client-" + this.random8String();
+        final String stackName = "elasticsearch-" + this.random8String();
+        try {
+            this.createStack(vpcStackName,"vpc/vpc-2azs.yaml");
+            try {
+                this.createStack(clientStackName,
+                        "state/client-sg.yaml",
+                        new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName)
+                );
+                try {
+                    this.createStack(stackName,
+                            "state/elasticsearch.yaml",
+                            new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName),
+                            new Parameter().withParameterKey("ParentClientStack").withParameterValue(clientStackName),
+                            new Parameter().withParameterKey("DomainName").withParameterValue(stackName)
+                    );
+                    // TODO how can we check if this stack works? start a bastion host and try to connect?
+                } finally {
+                    this.deleteStack(stackName);
+                }
+            } finally {
+                this.deleteStack(clientStackName);
+            }
+        } finally {
+            this.deleteStack(vpcStackName);
+        }
+    }
+
+}
