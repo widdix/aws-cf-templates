1d6ec052bee1a6641cab678cd5b68df03877c2cd
Michael Wittig <post@michaelwittig.info>
2018-06-27 14:31:36 +0200

    WIP: Amazon Linux 2 (#145)
    Amazon Linux 2

--- test/src/test/java/de/widdix/awscftemplates/staticwebsite/TestStaticWebsite.java
+++ test/src/test/java/de/widdix/awscftemplates/staticwebsite/TestStaticWebsite.java
@@ -13,57 +13,70 @@ public class TestStaticWebsite extends ACloudFormationTest {
 
     @Test
     public void test() {
+        final String zoneStackName = "zone-" + this.random8String();
         final String stackName = "static-website-" + this.random8String();
-        final String domainName = this.generateDomain(stackName);
-        final String redirectDomainName = this.generateDomain("www-" + stackName);
+        final String subDomainName = stackName;
+        final String redirectSubDomainName = "www-" + stackName;
         try {
-            this.createStack(stackName,
-                    "static-website/static-website.yaml",
-                    new Parameter().withParameterKey("DomainName").withParameterValue(domainName),
-                    new Parameter().withParameterKey("RedirectDomainName").withParameterValue(redirectDomainName),
-                    new Parameter().withParameterKey("CertificateType").withParameterValue("AcmCertificateArn"),
-                    new Parameter().withParameterKey("ExistingCertificate").withParameterValue(Config.get(Config.Key.CLOUDFRONT_ACM_CERTIFICATE_ARN)),
+            this.createStack(zoneStackName,
+                    "vpc/zone-legacy.yaml",
+                    new Parameter().withParameterKey("HostedZoneName").withParameterValue(Config.get(Config.Key.DOMAIN_SUFFIX)),
                     new Parameter().withParameterKey("HostedZoneId").withParameterValue(Config.get(Config.Key.HOSTED_ZONE_ID))
             );
-            final String url1 = "https://" + domainName + "/";
-            final String url2 = "https://" + domainName + "/folder/";
-            final String url3 = "https://" + redirectDomainName + "/";
             try {
-                this.createObject(domainName, "index.html", "hello");
-                this.createObject(domainName, "folder/index.html", "hello");
-                final Callable<HttpResponse> callable1 = () -> {
-                    final HttpResponse response = WS.url(url1).timeout(10000).get();
-                    // check HTTP response code
-                    if (WS.getStatus(response) != 200) {
-                        throw new RuntimeException("200 expected, but saw " + WS.getStatus(response));
-                    }
-                    return response;
-                };
-                final Callable<HttpResponse> callable2 = () -> {
-                    final HttpResponse response = WS.url(url2).timeout(10000).get();
-                    // check HTTP response code
-                    if (WS.getStatus(response) != 200) {
-                        throw new RuntimeException("200 expected, but saw " + WS.getStatus(response));
-                    }
-                    return response;
-                };
-                final Callable<HttpResponse> callable3 = () -> {
-                    final HttpResponse response = WS.url(url3).timeout(10000).followRedirect(false).get();
-                    // check HTTP response code
-                    if (WS.getStatus(response) != 301) {
-                        throw new RuntimeException("301 expected, but saw " + WS.getStatus(response));
-                    }
-                    return response;
-                };
-                this.retry(callable1);
-                this.retry(callable2);
-                this.retry(callable3);
+                this.createStack(stackName,
+                        "static-website/static-website.yaml",
+                        new Parameter().withParameterKey("ParentZoneStack").withParameterValue(zoneStackName),
+                        new Parameter().withParameterKey("SubDomainNameWithDot").withParameterValue(subDomainName + "."),
+                        new Parameter().withParameterKey("EnableRedirectSubDomainName").withParameterValue("true"),
+                        new Parameter().withParameterKey("RedirectSubDomainNameWithDot").withParameterValue(redirectSubDomainName + "."),
+                        new Parameter().withParameterKey("CertificateType").withParameterValue("AcmCertificateArn"),
+                        new Parameter().withParameterKey("ExistingCertificate").withParameterValue(Config.get(Config.Key.CLOUDFRONT_ACM_CERTIFICATE_ARN))
+                );
+                final String domainName = subDomainName + "." + Config.get(Config.Key.DOMAIN_SUFFIX);
+                final String redirectDomainName = redirectSubDomainName + "." + Config.get(Config.Key.DOMAIN_SUFFIX);
+                final String url1 = "https://" + domainName + "/";
+                final String url2 = "https://" + domainName + "/folder/";
+                final String url3 = "https://" + redirectDomainName + "/";
+                try {
+                    this.createObject(domainName, "index.html", "hello");
+                    this.createObject(domainName, "folder/index.html", "hello");
+                    final Callable<HttpResponse> callable1 = () -> {
+                        final HttpResponse response = WS.url(url1).timeout(10000).get();
+                        // check HTTP response code
+                        if (WS.getStatus(response) != 200) {
+                            throw new RuntimeException("200 expected, but saw " + WS.getStatus(response));
+                        }
+                        return response;
+                    };
+                    final Callable<HttpResponse> callable2 = () -> {
+                        final HttpResponse response = WS.url(url2).timeout(10000).get();
+                        // check HTTP response code
+                        if (WS.getStatus(response) != 200) {
+                            throw new RuntimeException("200 expected, but saw " + WS.getStatus(response));
+                        }
+                        return response;
+                    };
+                    final Callable<HttpResponse> callable3 = () -> {
+                        final HttpResponse response = WS.url(url3).timeout(10000).followRedirect(false).get();
+                        // check HTTP response code
+                        if (WS.getStatus(response) != 301) {
+                            throw new RuntimeException("301 expected, but saw " + WS.getStatus(response));
+                        }
+                        return response;
+                    };
+                    this.retry(callable1);
+                    this.retry(callable2);
+                    this.retry(callable3);
+                } finally {
+                    this.deleteObject(domainName, "folder/index.html");
+                    this.deleteObject(domainName, "index.html");
+                }
             } finally {
-                this.deleteObject(domainName, "folder/index.html");
-                this.deleteObject(domainName, "index.html");
+                this.deleteStack(stackName);
             }
         } finally {
-            this.deleteStackAndRetryOnFailure(stackName);
+            this.deleteStack(zoneStackName);
         }
     }
 
