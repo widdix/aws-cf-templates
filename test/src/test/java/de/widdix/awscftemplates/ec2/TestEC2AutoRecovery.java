7992feb2de629b79858413bec59daf4339754cf9
Michael Wittig <post@michaelwittig.info>
2019-01-08 09:19:45 +0100

    [Bug Fix] * - IAM SSH access was broken

--- test/src/test/java/de/widdix/awscftemplates/ec2/TestEC2AutoRecovery.java
+++ test/src/test/java/de/widdix/awscftemplates/ec2/TestEC2AutoRecovery.java
@@ -39,4 +39,35 @@ public class TestEC2AutoRecovery extends ACloudFormationTest {
         }
     }
 
+    @Test
+    public void testWithIAMUserSSHAccess() throws Exception {
+        final String vpcStackName = "vpc-2azs-" + this.random8String();
+        final String stackName = "ec2-auto-recovery-" + this.random8String();
+        final String classB = "10";
+        final String userName = "user-" + this.random8String();
+        try {
+            final User user = this.createUser(userName);
+            try {
+                this.createStack(vpcStackName,
+                        "vpc/vpc-2azs.yaml",
+                        new Parameter().withParameterKey("ClassB").withParameterValue(classB)
+                );
+                try {
+                    this.createStack(stackName,
+                            "ec2/ec2-auto-recovery.yaml",
+                            new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName),
+                            new Parameter().withParameterKey("IAMUserSSHAccess").withParameterValue("true")
+                    );
+                    final String host = this.getStackOutputValue(stackName, "IPAddress");
+                    this.probeSSH(host, user);
+                } finally {
+                    this.deleteStack(stackName);
+                }
+            } finally {
+                this.deleteStack(vpcStackName);
+            }
+        } finally {
+            this.deleteUser(userName);
+        }
+    }
 }
