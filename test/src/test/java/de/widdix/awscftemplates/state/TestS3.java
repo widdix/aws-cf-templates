cf653fa354f1d5984989e773f0bfe3632f959d2a
Michael Wittig <michael@widdix.de>
2020-10-21 09:56:19 +0200

    debug

--- test/src/test/java/de/widdix/awscftemplates/state/TestS3.java
+++ test/src/test/java/de/widdix/awscftemplates/state/TestS3.java
@@ -1,21 +1,22 @@
 package de.widdix.awscftemplates.state;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import de.widdix.awscftemplates.ACloudFormationTest;
 import de.widdix.awscftemplates.Context;
 import org.junit.Test;
 
-public class TestS3 extends ACloudFormationTest {
+public class TestS3 {
 
     @Test
     public void test() {
-        final Context context = new Context();
-        final String stackName = "s3-" + this.random8String();
-        try {
-            this.createStack(context, stackName, "state/s3.yaml");
-            // TODO how can we check if this stack works?
-        } finally {
-            this.deleteStack(context, stackName);
-        }
+       final AWSCredentialsProvider credentialsProvider = new DefaultAWSCredentialsProviderChain();
+       final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withCredentials(credentialsProvider).build();
+       System.out.println("buckets" + s3.listBuckets().size());
     }
 
 }
