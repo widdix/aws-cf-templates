6f4b2cd60d894247350cb1b0e6259b5dd5a77f83
Michael Wittig <michael@widdix.de>
2020-11-12 17:07:01 +0100

    fix tests

--- test/src/test/java/de/widdix/awscftemplates/vpc/AVPCTest.java
+++ test/src/test/java/de/widdix/awscftemplates/vpc/AVPCTest.java
@@ -7,10 +7,11 @@ import de.widdix.awscftemplates.Context;
 public abstract class AVPCTest extends ACloudFormationTest {
 
     protected final void testVPCSubnetInternetAccess(final Context context, final String parentVPCStack, final String subnetName) {
-        final String stackName = "al2-mutable-public-" + this.random8String();
+        final String reach = subnetName.endsWith("Private") ? "private" : "public";
+        final String stackName = "al2-mutable-" + reach + "-" + this.random8String();
         try {
             this.createStack(context, stackName,
-                    "ec2/al2-mutable-public.yaml",
+                    "ec2/al2-mutable-" + reach + ".yaml",
                     new Parameter().withParameterKey("ParentVPCStack").withParameterValue(parentVPCStack),
                     new Parameter().withParameterKey("SubnetName").withParameterValue(subnetName)
             );
