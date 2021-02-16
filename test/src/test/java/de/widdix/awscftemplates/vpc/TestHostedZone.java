a048a2fa1ad380afbc031c3fe0b38bfdf3aa2652
Andreas Wittig <andreas@widdix.de>
2021-02-16 21:52:52 +0100

    Adding test

---
+++ test/src/test/java/de/widdix/awscftemplates/vpc/TestHostedZone.java
@@ -0,0 +1,40 @@
+package de.widdix.awscftemplates.vpc;
+
+import com.amazonaws.services.cloudformation.model.Parameter;
+import de.widdix.awscftemplates.Context;
+import org.junit.Test;
+
+public class TestHostedZone extends ACloudFormationTest {
+
+    @Test
+    public void testDNSSEC() {
+        final Context context = new Context();
+        final String keyStackName = "key-" + this.random8String();
+        final String zoneStackName = "hostedzone-" + this.random8String();
+        final String dnssecStackName = "dnssec-" + this.random8String();
+        try {
+            this.createStack(context, keyStackName,"security/kms-key.yaml");
+            try {
+                this.createStack(context, zoneStackName,
+                        "vpc/zone-legacy.yaml",
+                        new Parameter().withParameterKey("HostedZoneName").withParameterValue(Config.get(Config.Key.DOMAIN_SUFFIX)),
+                        new Parameter().withParameterKey("HostedZoneId").withParameterValue(Config.get(Config.Key.HOSTED_ZONE_ID))
+                );
+                try {
+                    this.createStack(context, dnssecStackName,
+                            "vpc/zone-dnssec.yaml",
+                            new Parameter().withParameterKey("ParentZoneStack").withParameterValue(zoneStackName),
+                            new Parameter().withParameterKey("ParentKmsKeyStack").withParameterValue(keyStackName)
+                    );
+                } finally {
+                    this.deleteStack(context, dnssecStackName);
+                }
+            } finally {
+                this.deleteStack(context, zoneStackName);
+            }
+        } finally {
+            this.deleteStack(context, keyStackName);
+        }
+    }
+
+}
