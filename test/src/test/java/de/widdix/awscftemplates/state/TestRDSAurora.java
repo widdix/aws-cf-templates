9174bacb21b1737c722708bdb53ea2b4a5e4e69f
Michael Wittig <michael@widdix.de>
2021-04-09 12:44:44 +0200

    add tests

--- test/src/test/java/de/widdix/awscftemplates/state/TestRDSAurora.java
+++ test/src/test/java/de/widdix/awscftemplates/state/TestRDSAurora.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class TestRDSAurora extends ACloudFormationTest {
 
     @Test
-    public void testMySQL56() {
+    public void test() {
         final Context context = new Context();
         final String vpcStackName = "vpc-2azs-" + this.random8String();
         final String clientStackName = "client-" + this.random8String();
@@ -103,6 +103,45 @@ public class TestRDSAurora extends ACloudFormationTest {
                     // TODO how can we check if this stack works? start a bastion host and try to connect?
                 } finally {
                     this.deleteStack(context, stackName);
+                }
+            } finally {
+                this.deleteStack(context, clientStackName);
+            }
+        } finally {
+            this.deleteStack(context, vpcStackName);
+        }
+    }
+
+    @Test
+    public void testWithSecret() {
+        final Context context = new Context();
+        final String vpcStackName = "vpc-2azs-" + this.random8String();
+        final String clientStackName = "client-" + this.random8String();
+        final String secretStackName = "secret-" + this.random8String();
+        final String stackName = "rds-aurora-" + this.random8String();
+        try {
+            this.createStack(context, vpcStackName, "vpc/vpc-2azs.yaml");
+            try {
+                this.createStack(context, clientStackName,
+                        "state/client-sg.yaml",
+                        new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName)
+                );
+                try {
+                    this.createStack(context, secretStackName,"state/secretsmanager-dbsecret.yaml");
+                    try {
+                        this.createStack(context, stackName,
+                                "state/rds-aurora.yaml",
+                                new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName),
+                                new Parameter().withParameterKey("ParentClientStack").withParameterValue(clientStackName),
+                                new Parameter().withParameterKey("ParentSecretStack").withParameterValue(secretStackName),
+                                new Parameter().withParameterKey("DBName").withParameterValue("db1")
+                        );
+                        // TODO how can we check if this stack works? start a bastion host and try to connect?
+                    } finally {
+                        this.deleteStack(context, stackName);
+                    }
+                } finally {
+                    this.deleteStack(context, secretStackName);
                 }
             } finally {
                 this.deleteStack(context, clientStackName);
