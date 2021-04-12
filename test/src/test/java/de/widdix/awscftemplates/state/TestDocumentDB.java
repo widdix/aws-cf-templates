0d34548f04ae11cac103dc3cb7777b01ea612196
Michael Wittig <michael@widdix.de>
2021-04-12 08:07:13 +0200

    [Improvement] state/* - Add Secrets Manager Secret support (#544)
    Co-authored-by: Markus Ellers <markus.ellers@cloudbuddies.de>
    Co-authored-by: Markus Ellers <shotty1@users.noreply.github.com>

--- test/src/test/java/de/widdix/awscftemplates/state/TestDocumentDB.java
+++ test/src/test/java/de/widdix/awscftemplates/state/TestDocumentDB.java
@@ -38,4 +38,43 @@ public class TestDocumentDB extends ACloudFormationTest {
             this.deleteStack(context, vpcStackName);
         }
     }
+
+    @Test
+    public void testWithSecret() {
+        final Context context = new Context();
+        final String vpcStackName = "vpc-2azs-" + this.random8String();
+        final String clientStackName = "client-" + this.random8String();
+        final String secretStackName = "secret-" + this.random8String();
+        final String stackName = "documentdb-" + this.random8String();
+        try {
+            this.createStack(context, vpcStackName, "vpc/vpc-2azs.yaml");
+            try {
+                this.createStack(context, clientStackName,
+                        "state/client-sg.yaml",
+                        new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName)
+                );
+                try {
+                    try {
+                        this.createStack(context, secretStackName,"state/secretsmanager-dbsecret.yaml");
+                        this.createStack(context, stackName,
+                                "state/documentdb.yaml",
+                                new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName),
+                                new Parameter().withParameterKey("ParentClientStack").withParameterValue(clientStackName),
+                                new Parameter().withParameterKey("ParentSecretStack").withParameterValue(secretStackName)
+                        );
+                        // TODO how can we check if this stack works? start a bastion host and try to connect?
+                    } finally {
+                        this.deleteStack(context, stackName);
+                    }
+                } finally {
+                    this.deleteStack(context, secretStackName);
+                }
+            } finally {
+                this.deleteStack(context, clientStackName);
+            }
+        } finally {
+            this.deleteStack(context, vpcStackName);
+        }
+    }
+
 }
--- test/src/test/java/de/widdix/awscftemplates/state/TestRDSAuroraServerlessPostgres.java
+++ test/src/test/java/de/widdix/awscftemplates/state/TestRDSAuroraServerlessPostgres.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 public class TestRDSAuroraServerlessPostgres extends ACloudFormationTest {
 
     @Test
-    public void testPostgres107() {
+    public void test() {
         final Context context = new Context();
         final String vpcStackName = "vpc-2azs-" + this.random8String();
         final String clientStackName = "client-" + this.random8String();
@@ -37,6 +37,52 @@ public class TestRDSAuroraServerlessPostgres extends ACloudFormationTest {
                     } finally {
                         this.deleteStack(context, stackName);
                     }} finally {
+                    this.deleteStack(context, kmsKeyStackName);
+                }
+            } finally {
+                this.deleteStack(context, clientStackName);
+            }
+        } finally {
+            this.deleteStack(context, vpcStackName);
+        }
+    }
+
+    @Test
+    public void testWithSecret() {
+        final Context context = new Context();
+        final String vpcStackName = "vpc-2azs-" + this.random8String();
+        final String clientStackName = "client-" + this.random8String();
+        final String kmsKeyStackName = "key-" + this.random8String();
+        final String secretStackName = "secret-" + this.random8String();
+        final String stackName = "rds-aurora-serverless-postgres-" + this.random8String();
+        try {
+            this.createStack(context, vpcStackName, "vpc/vpc-2azs.yaml");
+            try {
+                this.createStack(context, clientStackName,
+                        "state/client-sg.yaml",
+                        new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName)
+                );
+                try {
+                    this.createStack(context, kmsKeyStackName,"security/kms-key.yaml");
+                    try {
+                        this.createStack(context, secretStackName,"state/secretsmanager-dbsecret.yaml");
+                        try {
+                            this.createStack(context, stackName,
+                                    "state/rds-aurora-serverless-postgres.yaml",
+                                    new Parameter().withParameterKey("ParentVPCStack").withParameterValue(vpcStackName),
+                                    new Parameter().withParameterKey("ParentClientStack").withParameterValue(clientStackName),
+                                    new Parameter().withParameterKey("ParentKmsKeyStack").withParameterValue(kmsKeyStackName),
+                                    new Parameter().withParameterKey("ParentSecretStack").withParameterValue(secretStackName),
+                                    new Parameter().withParameterKey("DBName").withParameterValue("db1")
+                            );
+                            // TODO how can we check if this stack works? start a bastion host and try to connect?
+                        } finally {
+                            this.deleteStack(context, stackName);
+                        }
+                    } finally {
+                        this.deleteStack(context, secretStackName);
+                    }
+                } finally {
                     this.deleteStack(context, kmsKeyStackName);
                 }
             } finally {
