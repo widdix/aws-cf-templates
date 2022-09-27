85843a5503d439b861e195f3d8121fb6072e3148
Michael Wittig <michael@widdix.de>
2022-09-27 13:15:27 +0200

    fix

--- test/src/test/java/de/widdix/awscftemplates/state/TestRDSAuroraServerlessPostgres.java
+++ test/src/test/java/de/widdix/awscftemplates/state/TestRDSAuroraServerlessPostgres.java
@@ -84,7 +84,7 @@ public class TestRDSAuroraServerlessPostgres extends ACloudFormationTest {
                                     new Parameter().withParameterKey("ParentKmsKeyStack").withParameterValue(kmsKeyStackName),
                                     new Parameter().withParameterKey("ParentSecretStack").withParameterValue(secretStackName),
                                     new Parameter().withParameterKey("DBName").withParameterValue("db1"),
-                                    new Parameter().withParameterKey("EngineVersion").withParameterValue("10.7")
+                                    new Parameter().withParameterKey("EngineVersion").withParameterValue("10.18")
                             );
                             // TODO how can we check if this stack works? start a bastion host and try to connect?
                         } finally {
